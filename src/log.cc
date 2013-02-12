74596fa88a527ef0d8725dfc5a5d613bee6c744a
Jeff Weisberg <jaw@solvemedia.com>
2013-02-12 12:49:43 -0500

    whitespace

--- src/log.cc
+++ src/log.cc
@@ -79,13 +79,13 @@ log_request(NTD *ntd){
     }
 
 
-    fprintf(f, " %s %s/%d-%d %x %d ",
+    fprintf(f, " %s %s/%d-%d %x %d",
             (ntd->querb.bufsize == UDPBUFSIZ) ? "udp" : "tcp",
             ntd->querd.name, ntd->querd.klass, ntd->querd.type,
             ntd->respd.flags & ~FLAG_RESPONSE, ntd->respb.datalen);
 
     if( ntd->edns.udpsize ){
-        fprintf(f, "edns %d ", ntd->edns.udpsize);
+        fprintf(f, " edns %d ", ntd->edns.udpsize);
     }
 
     switch( ntd->edns.addr_family ){
